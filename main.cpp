6f9cd54f9d8466944177e8d0c40c74a276ef1441
Denis <534441.0a@gmail.com>
2025-11-22 15:45:49 +0300

    Add print round statistic

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@
 #include <random>
 #include <string>
 #include <system_error>
+#include <string_view>
 
 constexpr int FINAL_ROUND = 10;
 constexpr int MAX_PERCENT_DEAD_CITIZEN = 45;  // от голода
@@ -322,6 +323,94 @@ int32_t read_num_acre_to_plant_with_retry(
       });
 }
 
+void print_message_to_ruler(RoundState const& rs) {
+    // год
+    // голод, прибыли
+    // чума
+    // население сейчас
+    // собрали пшеницы с пшеницей с акра
+    // крысы истребили, оставили в амбарах
+    // сколько акров занимает город
+    // стоимость акра
+    const std::string tmpl = "Мой повелитель, соизволь поведать тебе "
+        "в году {} твоего высочайшего правления\n"
+        "{}, и {};\n"
+        "{}\n"
+        "{}\n"
+        "{}\n"
+        "{}\n"
+        "{}\n"
+        "{}\n"
+        "Что пожелаешь, повелитель?\n";
+
+    CityState const& cs = rs.city_state;
+    CityEvents const& ce = rs.city_events.value();
+
+    std::string dead_citizen_str;
+    if (ce.num_dead_citizen > 0) {
+      dead_citizen_str = std::format("{} человек умерли с голоду,", ce.num_dead_citizen);
+    }
+    else
+    {
+      dead_citizen_str = "никто не умер с голоду";
+    }
+
+    std::string new_citizen_str;
+    if (ce.num_new_citizen > 0) {
+      new_citizen_str =
+          std::format("{} человек прибыли в наш великий город", ce.num_new_citizen);
+    } else {
+      dead_citizen_str = "никто не прибыл в наш город";
+    }
+
+    std::string plague_str;
+    if (ce.plague) {
+      plague_str = "Чума уничтожила половину населения;";
+    }
+
+    std::string num_citizen_str =
+        std::format("Население города сейчас составляет {} человек;",
+                    cs.num_citizen + ce.num_new_citizen - ce.num_dead_citizen);
+
+    std::string new_bushels_str;
+    if (cs.num_acre_with_wheat > 0) {
+      new_bushels_str = std::format(
+          "Мы собрали {} бушелей пшеницы, по {} бушеля с акра;",
+          get_num_cultivate_bushels(cs, ce), ce.bushels_wheat_per_acre);
+    } else {
+      new_bushels_str =
+          "Мы не засадили ни одного акра, поэтому ничего не собрали";
+    }
+
+    int32_t bushels_wheat = cs.bushels_wheat +
+                            get_num_cultivate_bushels(cs, ce) -
+                            ce.bushels_wheat_eaten_rats;
+
+    std::string eat_bushels_str;
+    if (ce.bushels_wheat_eaten_rats > 0) {
+      eat_bushels_str = std::format(
+          "Крысы истребили {} бушелей пшеницы, оставив {} бушеля в амбарах;",
+          ce.bushels_wheat_eaten_rats, bushels_wheat);
+    } else {
+      eat_bushels_str =
+          std::format("Крысы не потревожили нас, поэтому осталось {} бушеля в амбарах;",
+                      bushels_wheat);
+    }
+
+    std::string num_acre_str = std::format("Город сейчас занимает {} акров;", cs.num_acre);
+
+    std::string price_acre_str =
+        std::format("1 акр земли стоит сейчас {} бушель;", ce.acre_price);
+
+    auto msg = std::vformat(
+        std::string_view(tmpl),
+        std::make_format_args(rs.num_round, dead_citizen_str, new_citizen_str, plague_str,
+                              num_citizen_str, new_bushels_str, eat_bushels_str,
+                              num_acre_str, price_acre_str));
+
+    std::println("{}", msg);
+}
+
 RulersDecisions get_rulers_decisions(std::istream& in,
                                      RoundState const& round_state) {
   RulersDecisions rd{
@@ -443,12 +532,16 @@ void run_game() {
     current_round.city_events =
         gen_new_city_events(seed, current_round.city_state);
 
-    std::print("current_round after events: {}\n", current_round);
+    print_message_to_ruler(current_round);
+
+    std::println("current_round after events: {}", current_round);
+
+    return;
 
     current_round.rulers_decisions =
         get_rulers_decisions(std::cin, current_round);
 
-    std::print("current_round: {}\n", current_round);
+    std::println("current_round: {}", current_round);
   }
 
   // TODO Считаем статистики P и L
